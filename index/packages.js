a08c21a684f6fa03e52d9b4886a92adf703092ae
thought2 <no-day@posteo.net>
2021-12-30 12:43:50 +0000

    Deploying to gh-pages from @ thought2/purescript-purenix-miraculix@d59ccb1b739c219a7ea03e1c1e6a5954e59aa27b 🚀

---
+++ index/packages.js
@@ -0,0 +1,1 @@
+window.DocsSearchPackageIndex = [{"score":0,"repository":"https://github.com/purescript/purescript-identity.git","name":"identity","description":null,"dependencies":["control","invariant","newtype","prelude"]}]
