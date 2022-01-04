47bb5422953b9a9eab6c15b1ca10de4d2456d2e3
Michael Bock <me@thought2.de>
2022-01-04 13:39:14 +0100

    Start with flake

--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
 
   inputs.purenix.url = "github:purenix-org/purenix";
   inputs.nixpkgs.follows = "purenix/nixpkgs";
+  inputs.easy-purescript-nix-source = {
+    url = "github:justinwoo/easy-purescript-nix";
+    flake = false;
+  };
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils, purenix }:
+  outputs = { self, nixpkgs, flake-utils, purenix, easy-purescript-nix-source }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlay = self: _: { };
@@ -16,11 +20,16 @@
           ];
         };
 
+        easy-purescript-nix = import easy-purescript-nix-source { inherit pkgs; };
+
       in
       {
+        defaultPackage = import ./nix/pkgs/miraculix.nix { inherit pkgs; };
+
         devShell = pkgs.mkShell {
           nativeBuildInputs = [
             purenix.defaultPackage.${system}
+            easy-purescript-nix.spago2nix
             pkgs.purescript
             pkgs.spago
             pkgs.nodePackages.purescript-psa
---
+++ nix/pkgs/miraculix.nix
@@ -0,0 +1,56 @@
+{ pkgs }:
+let
+  spagoPkgs = import ../../spago-packages.nix { inherit pkgs; };
+
+  getGlob = pkg: ''".spago/${pkg.name}/${pkg.version}/src/**/*.purs"'';
+
+  buildSpagoStyle = pkgs.writeShellScriptBin "build-spago-style" ''
+    set -e
+    echo building project...
+    ${pkgs.purenix}/bin/purenix \
+    ${builtins.toString (builtins.map getGlob (builtins.attrValues spagoPkgs.inputs))} "$@"
+    echo done.
+  '';
+
+  output = pkgs.stdenv.mkDerivation
+    rec {
+      pname = "miraculix";
+      src = pkgs.lib.cleanSource ../../src;
+      version = "0.2.0";
+
+      buildInputs = [
+        spagoPkgs.installSpagoStyle
+        spagoPkgs.buildSpagoStyle
+        spagoPkgs.buildFromNixStore
+        pkgs.purescript
+        pkgs.spago
+      ];
+
+      unpackPhase = ''
+        cp ${../..}/{packages,spago}.dhall .
+        cp -r $src/src .
+        install-spago-style
+      '';
+
+      buildPhase = ''
+        build-spago-style './src/**/*.purs' -g corefn
+        ${pkgs.purenix}/bin/purenix
+        mkdir $out
+        cp -r output/* -t $out
+      '';
+
+      installPhase = ''
+      
+      '';
+    };
+
+  defaultNix = pkgs.writeText "default.nix" ''
+    import ./output/Main/default.nix
+  '';
+
+in
+pkgs.runCommand "output-with-extra" { } ''
+  mkdir $out
+  ln -s ${output} $out/output
+  ln -s ${defaultNix} $out/default.nix
+''
