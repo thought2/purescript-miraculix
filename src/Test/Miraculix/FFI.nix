97167d62a08bb5f134ec5615602dffcba538d764
thought2 <no-day@posteo.net>
2022-06-05 10:38:26 +0000

    Deploying to lite from @ thought2/purescript-miraculix@58bf94378b45b093c05d869af4108ba74838f1ec 🚀

--- src/Test/Miraculix/FFI.nix
+++ src/Test/Miraculix/FFI.nix
@@ -1,5 +1,11 @@
 let
-  pkgs = import <nixpkgs> { };
+  pkgs = import
+    (fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/fddafcc062e4fbea7988479407edec4f837b5061.tar.gz";
+      sha256 = "0b6ky46s0wdwhrg9yp5jh0dq644c5cmlibbvqsjfq031b4jzk5l6";
+    })
+    { localSystem = "x86_64-linux"; }
+  ;
   mkReport = txt: pkgs.writeText "test-success" txt;
 in
 {
